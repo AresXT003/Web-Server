4331c3b4c1691a8350a766bb8852e86f85310d1c
AresXT <160696864+AresXT003@users.noreply.github.com>
2024-03-05 13:05:41 +0530

    Update secrets.h

--- accespoint/secrets.h
+++ accespoint/secrets.h
@@ -1,8 +1,8 @@
-// #define SECRET_SSID "Grapes"
-// #define SECRET_OPTIONAL_PASS "Admin@123"
+// #define SECRET_SSID "SSID"
+// #define SECRET_OPTIONAL_PASS "PASSWORD"
 #define SECRET_USERNAME "admin"
 #define SECRET_PASS "password"
 // #ifndef APSSID
-#define APSSID "Grapes Test"
+#define APSSID "Test"
 #define APPSK "123456789"
 //  #endif
